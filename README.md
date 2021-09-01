08bd48b3a431ed89e994c3d898b7a9b2d7b7d69a
Luis Carrillo <luis@d3v.mx>
2021-09-01 16:18:14 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dotnet-captcha
 dotnet 5 | captcha | drawing
+
+```
+# .\src\Application
+dotnet new classlib -o .\src\Core\Application -f net5.0
+dotnet add .\src\Core\Application package Microsoft.Extensions.DependencyInjection.Abstractions
+dotnet add .\src\Core\Application package System.Drawing.Common
+rm .\src\Core\Application\Class1.cs
+mkdir .\src\Core\Application\Common
+mkdir .\src\Core\Application\Repositories
+mkdir .\src\Core\Application\Services
+echo 'namespace Application.Common { }' > .\src\Core\Application\Common\Exceptions.cs
+echo 'namespace Application.Common { }' > .\src\Core\Application\Common\IDbContext.cs
+echo 'namespace Application.Common { }' > .\src\Core\Application\Common\IRepository.cs
+echo 'namespace Application.Services { }' > .\src\Core\Application\Services\CaptchaService.cs
+echo 'namespace Application { }' > .\src\Core\Application\DependencyInjection.cs
+
+# .\src\Presentation
+dotnet new mvc -o .\src\Presentation\WebApp -f net5.0
+dotnet add .\src\Presentation\WebApp reference .\src\Core\Application
+
+dotnet new sln
+dotnet sln add (ls -r .\**\*.csproj)
+```
---
+++ src/Presentation/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ICaptchaService _captchaService;
+
+        public HomeController(ILogger<HomeController> logger,
+                  ICaptchaService captchaService)
+        {
+            _logger = logger;
+            _captchaService = captchaService;
+        }
+
+        public IActionResult Index()
+        {
+            var text = _captchaService.RandomString(5, RandomCharactersType.All);
+            TempData["CaptchaCode"] = text;
+            ViewData["CaptchaImage"] = _captchaService.CreateCaptcha(text);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string captcha)
+        {
+            var captchaCode = TempData["CaptchaCode"].ToString();
+
+            if (string.IsNullOrEmpty(captchaCode) || captcha != captchaCode)
+            {
+                return StatusCode(500, "Error de Captcha");
+            }
+
+            return Ok("Captcha aceptado");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
